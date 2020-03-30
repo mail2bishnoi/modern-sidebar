184e257cf6d709d4fb1e4f01b0ff07144f26e9e2
Ashutosh Bishnoi <mail2bishnoi@gmail.com>
2020-03-30 17:11:53 +0530

    Updates

---
+++ precache-manifest.2dad0fd0925a6745f55770c0fc745094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b07e5de8fc3ef207eab96de43f435bf",
+    "url": "/modern-sidebar/index.html"
+  },
+  {
+    "revision": "b05e5c492225ca202632",
+    "url": "/modern-sidebar/static/css/main.826dfa4a.chunk.css"
+  },
+  {
+    "revision": "3e31d66a4163018400d4",
+    "url": "/modern-sidebar/static/js/2.41253e55.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/modern-sidebar/static/js/2.41253e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05e5c492225ca202632",
+    "url": "/modern-sidebar/static/js/main.dc2576f7.chunk.js"
+  },
+  {
+    "revision": "ecfbd135f692a39ee6c5",
+    "url": "/modern-sidebar/static/js/runtime-main.6ce8af23.js"
+  }
+]);
